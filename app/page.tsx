0904f20d3aec66910a59c3e7a0c5026442ac7e5a
Tushar Konala <tusharkonala284@gmail.com>
2025-12-19 12:07:56 +0530

    Add ProductCard and AddToCart components to enhance product display

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import ProductCard from "./components/ProductCard";
 
 export default function Home() {
   return (
@@ -7,6 +8,7 @@ export default function Home() {
       <main>
         <h1>Hello World</h1>
         <Link href="/users">Users</Link>
+        <ProductCard />
       </main>
     </div>
   );
