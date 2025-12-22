f9f7edbde6537a7a1fa293b0c65420a578b6a49f
Tushar Konala <tusharkonala284@gmail.com>
2025-12-22 18:00:03 +0530

    Implemented styles using css modules

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import ProductCard from "./components/ProductCard";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <div className="flex min-h-screen bg-zinc-50 font-sans dark:bg-black">
       <main>
         <h1>Hello World</h1>
         <Link href="/users">Users</Link>
