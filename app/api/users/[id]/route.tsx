607abcd5c103a8df2ae093477919c7f46dff5066
Tushar Konala <tusharkonala284@gmail.com>
2026-01-02 13:40:30 +0530

    Implemented req body validation using zod

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -1,4 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
+import schema from "../schema";
 
 export async function GET(
   request: NextRequest,
@@ -18,13 +19,15 @@ export async function GET(
 
 export async function PUT(
   request: NextRequest,
-  { params }: { params: { id: number } }
+  { params }: { params: Promise<{ id: number }> }
 ) {
   const body = await request.json();
   const { id } = await params;
 
-  if (!body.name) {
-    return NextResponse.json({ error: "Name is required" }, { status: 400 });
+  const validation = schema.safeParse(body);
+
+  if (!validation.success) {
+    return NextResponse.json(validation.error.message, { status: 400 });
   }
 
   if (id > 10) {
@@ -32,4 +35,16 @@ export async function PUT(
   }
 
   return NextResponse.json({ id: 1, name: body.name });
+}
+
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: { id: number } }
+) {
+  const { id } = await params;
+  if (id > 10) {
+    return NextResponse.json({ error: "User not found" }, { status: 400 });
+  }
+
+  return NextResponse.json({});
 }
