9ae5d574f0fb3526fa2f52605ae0ec571f3779fe
Tushar Konala <tusharkonala284@gmail.com>
2026-01-11 22:48:20 +0530

    Implementing google tag manager

---
+++ app/GoogleAnalyticsScript.tsx
@@ -0,0 +1,22 @@
+import Script from "next/script";
+
+const GoogleAnalyticsScript = () => {
+  return (
+    <>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-E720JHXSJ2"
+      />
+      <Script id="google-analytics">
+        {`window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-E720JHXSJ1');`}
+      </Script>
+      {/* look for startegy prop of <Script/> */}
+    </>
+  );
+};
+
+export default GoogleAnalyticsScript;
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NavBar from "./NavBar";
 import AuthProvider from "./auth/Provider";
+import GoogleAnalyticsScript from "./GoogleAnalyticsScript";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,6 +27,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" data-theme="luxury">
+      {/* <GoogleAnalyticsScript /> */}
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
