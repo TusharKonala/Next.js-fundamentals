2649a1fa4040a85a44fd46e18ab9d66516b1f453
Tushar Konala <tusharkonala284@gmail.com>
2026-01-11 20:17:57 +0530

    Sending emails using Resend

--- emails/WelcomeTemplate.tsx
+++ emails/WelcomeTemplate.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { CSSProperties } from "react";
 import {
   Html,
   Body,
@@ -6,20 +6,34 @@ import {
   Text,
   Link,
   Preview,
+  Tailwind,
 } from "@react-email/components";
 
 const WelcomeTemplate = ({ name }: { name: string }) => {
   return (
     <Html>
       <Preview>Welcome Aboard!</Preview>
-      <Body>
-        <Container>
-          <Text>Hello {name}</Text>
-          <Link href="https://codewithmosh.com">www.codewithmosh.com</Link>
-        </Container>
-      </Body>
+      <Tailwind>
+        {/* <Body style={body}> */}
+        <Body className="bg-white">
+          <Container>
+            <Text className="font-bold text-3xl" style={heading}>
+              Hello {name}
+            </Text>
+            <Link href="https://codewithmosh.com">www.codewithmosh.com</Link>
+          </Container>
+        </Body>
+      </Tailwind>
     </Html>
   );
+};
+
+const body: CSSProperties = {
+  background: "#fff",
+};
+
+const heading: CSSProperties = {
+  fontSize: "32px",
 };
 
 export default WelcomeTemplate;
