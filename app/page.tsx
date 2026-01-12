2913238415c1e256bf7e42a877295b0849c37409
Tushar Konala <tusharkonala284@gmail.com>
2026-01-12 21:17:20 +0530

    Using fonts in next.js

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,17 @@
-import Image from "next/image";
-import coffee from "@/public/images/coffee.jpg";
+import Link from "next/link";
+import ProductCard from "./components/ProductCard";
+import { getServerSession } from "next-auth";
+import { authOptions } from "./api/auth/[...nextauth]/route";
 
 export default async function Home() {
+  const session = await getServerSession(authOptions);
   return (
-    <main className="relative h-screen">
-      {/* <Image src={coffee} alt="Coffee" /> */}
-      <Image
-        src="https://bit.ly/react-cover"
-        alt="Coffee"
-        fill
-        className="object-cover"
-        sizes="(max-width: 480px) 100vw, (max-width: 768px) 50vw, 33vw"
-        quality={100}
-        priority
-      />
-    </main>
+    <div className="flex min-h-screen bg-zinc-50 font-sans dark:bg-black">
+      <main>
+        <h1>Hello World {session && <span>{session.user!.name}</span>}</h1>
+        <Link href="/users">Users</Link>
+        <ProductCard />
+      </main>
+    </div>
   );
 }
