3ed2b49be75cae2ee8dfca6ec875f0d1e51bcfba
Tushar Konala <tusharkonala284@gmail.com>
2026-01-12 21:34:19 +0530

    Adding meta data

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import ProductCard from "./components/ProductCard";
 import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]/route";
+import { Metadata } from "next";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
@@ -9,9 +10,21 @@ export default async function Home() {
     <div className="flex min-h-screen bg-zinc-50 font-sans dark:bg-black">
       <main>
         <h1>Hello World {session && <span>{session.user!.name}</span>}</h1>
-        <Link href="/users">Users</Link>
         <ProductCard />
       </main>
+      <Link href="/users">Users</Link>
     </div>
   );
 }
+
+// export const metaData: Metadata = {
+//   title: "...",
+// };
+
+// export async function generateMetaData(): Promise<Metadata> {
+//   const product = await fetch("");
+//   return {
+//     title: product.title,
+//     description: "...",
+//   };
+// }
