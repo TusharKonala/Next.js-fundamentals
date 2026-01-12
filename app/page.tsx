01d0666e27df4e6e723c10ce60516493fc5f88ad
Tushar Konala <tusharkonala284@gmail.com>
2026-01-12 22:07:47 +0530

    Lazy loading a component

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,28 @@
+"use client";
+import { useState } from "react";
+import dynamic from "next/dynamic";
+const HeavyComponent = dynamic(() => import("./components/HeavyComponent"), {
+  ssr: false,
+  loading: () => <p>Loading...</p>,
+});
+// import HeavyComponent from "./components/HeavyComponent";
 import Link from "next/link";
 import ProductCard from "./components/ProductCard";
 import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]/route";
 import { Metadata } from "next";
 
-export default async function Home() {
-  const session = await getServerSession(authOptions);
+export default function Home() {
+  // const session = await getServerSession(authOptions);
+  const [isVisible, setVisible] = useState(false);
   return (
     <div className="flex min-h-screen bg-zinc-50 font-sans dark:bg-black">
       <main>
-        <h1>Hello World {session && <span>{session.user!.name}</span>}</h1>
+        {/* <h1>Hello World {session && <span>{session.user!.name}</span>}</h1> */}
+        <h1>Hello World</h1>
+        <button onClick={() => setVisible(true)}>Show</button>
         <ProductCard />
+        {isVisible && <HeavyComponent />}
       </main>
       <Link href="/users">Users</Link>
     </div>
